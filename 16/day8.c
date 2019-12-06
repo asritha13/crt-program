3c56a379b3a0c4d13a2d7bb6011bea7e83974a40
asritha13 <58385021+asritha13@users.noreply.github.com>
2019-12-06 12:45:20 +0530

    Add files via upload

---
+++ 16/day8.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+main()
+{
+	int n;
+	printf("\n enter a number:");
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("\n %d is positive",n);
+	}
+	else if(n<0)
+	{
+		printf("\n %d is negative",n);
+	}
+	else
+	
+	{
+		printf("0");
+	}
+}
---
+++ 16/even or odd.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+Float avg=65.123456;
+printf("\n average:%$.3f",avg);
+printf("\"Hello World\"");
+printf("\n GITAM\’s college");
+}
+
+
